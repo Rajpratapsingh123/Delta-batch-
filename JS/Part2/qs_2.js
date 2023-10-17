7b7736cd1cda77dc91ce1785df54646a380a92f4
Rajpratapsingh123 <rajpratapsingh471@gmail.com>
2023-10-17 23:19:26 +0530

    Question 2

---
+++ JS/Part2/qs_2.js
@@ -0,0 +1,13 @@
+// QS 2.   Take the user's name & age as input using prompts. Then return back the following statement to the user as an alert (by substituting their name & age):
+// name is age year old.
+
+
+let name  = prompt("Enter your name") ;
+let age=prompt("Enter your age");
+
+
+
+
+
+console.log(alert(`${name} is ${age} year old`));
+
