7f25995f5c9b54ecfc00353157d8077ba5485208
Rajpratapsingh123 <rajpratapsingh471@gmail.com>
2023-10-21 01:34:56 +0530

    Question 1

--- JS/Part_3/qs_1.js
+++ JS/Part_3/qs_1.js
@@ -3,11 +3,13 @@
 
 
 
-let arr=["january","July","March","August"];
+ let arr=["january","July","March","August"];
 
-arr.shift();
-arr.shift();
-//console.log(arr);
-arr.unshift("June");
-arr.unshift("July");
-console.log(arr);
+ arr.shift();
+ arr.shift();
+ console.log(arr);
+ arr.unshift("June");
+ arr.unshift("July");
+ console.log(arr);
+
+
