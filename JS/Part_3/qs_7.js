b02a972a763d349594e7784be67fad1905770b08
Rajpratapsingh123 <rajpratapsingh471@gmail.com>
2023-10-23 23:49:54 +0530

    Question 7

---
+++ JS/Part_3/qs_7.js
@@ -0,0 +1,15 @@
+// Write a JS program to to test whether the character at the given (character) index is lower case.
+
+
+
+
+
+let str=("Pandey");
+let idx =3;
+
+if(str[idx]==str[idx].toLowerCase()){
+    console.log("It is lowercase");
+}
+else{
+    console.log("It is not lowercase");
+}
